90a86036a7247a8f962fe833626385f6792eecf5
Yasmine Hajaji <yasminehajaji95@gmail.com>
2019-09-13 22:02:20 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-
+def key_for_min_vame_hash)
+name_hash.cot4=log10
 end
